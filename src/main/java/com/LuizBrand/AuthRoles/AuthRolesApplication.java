0055f5808f97ad8f331faff0349fe49494aea4da
Luiz <lbrandaolira@gmail.com>
2025-07-16 20:15:45 -0400

    Initial commit

---
+++ src/main/java/com/LuizBrand/AuthRoles/AuthRolesApplication.java
@@ -0,0 +1,13 @@
+package com.LuizBrand.AuthRoles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthRolesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LuizBrand/AuthRoles/AuthRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LuizBrand.AuthRoles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthRolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
